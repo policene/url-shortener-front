18dc4595809f7778548d01aa17eb69723f9dd3e8
Giovanni <giovannicpoli@gmail.com>
2026-05-02 19:38:21 -0300

    fix: variavel de ambiente

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.API_URL;
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export async function shortenUrl(url: string): Promise<string> {
   const res = await fetch(`${BASE_URL}/shorten`, {
